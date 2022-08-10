bc46d62ad4d0185b91efcb7da622d83d6b6cfd94
Josh Reiten <joshreitencode@gmail.com>
2022-08-09 20:07:46 -0500

    Added models

---
+++ todoapp/src/main/java/com/juggernauts/todoapp/models/Task.java
@@ -0,0 +1,16 @@
+package com.juggernauts.todoapp.models;
+
+
+import java.util.Date;
+
+public class Task {
+    private int id;
+    private String name;
+    private String category;
+    private Date completeBy;
+    private boolean isDone;
+    private String description;
+    private int repeatsEvery;
+    private int remindBy;
+}
+
