598d3769480ca17c10002dcbe29f309d071b1eb3
wallace <thewallacems@gmail.com>
2022-08-17 12:49:25 -0400

    add twitter API

--- todoapp/src/main/java/com/juggernauts/todoapp/TodoappApplication.java
+++ todoapp/src/main/java/com/juggernauts/todoapp/TodoappApplication.java
@@ -11,9 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.juggernauts.todoapp.repos")
 @EnableFeignClients
 public class TodoappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
 	}
-
 }
---
+++ todoapp/src/main/java/com/juggernauts/todoapp/utils/Tweet.java
@@ -0,0 +1,49 @@
+package com.juggernauts.todoapp.utils;
+
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+/**
+ * git branch: Twitter_API
+ *
+ * Twitter4J/ wrapper for Twitter API V2
+ * Tweet object POSTs tweet to share ToDoApp
+ * for Team Juggernaut, by way of Habit.ual
+ */
+
+public class Tweet {
+
+    public Tweet(){}
+
+    String tw = "I stay on task with the ToDoApp from Habit.ual. Download the app today!";
+
+
+    public String shareApp(String tw){
+
+        System.out.println();
+        System.out.println("*************** TWEET in Progress *********************");
+
+        String API_KEY = "foo";
+        String API_KEY_SECRET ="foo";
+        String ACCESS_TOKEN = "foo-bar";
+        String ACCESS_TOKEN_SECRET = "foo";
+
+        try {
+            twitter4j.Twitter twitter = new TwitterFactory().getInstance();
+
+            // twitter.setOAuthConsumer(API_KEY, API_KEY_SECRET);
+
+            AccessToken accessToken = new AccessToken(ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+
+            twitter.setOAuthAccessToken(accessToken);
+
+            twitter.updateStatus(tw);
+            System.out.println();
+            System.out.println("Successfully updated the status in Twitter.");
+        } catch (TwitterException te) {
+            te.printStackTrace();
+        }
+        return "Successfully updated the status in Twitter.";
+    }
+}
